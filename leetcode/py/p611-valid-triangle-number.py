0ef488acf3182d32385021cfe5a1ebc0ede53bb4
Matt Drees <matt@drees.me>
2021-07-23 17:22:04 -0700

    Updated LeetCode p611. Valid Triangle Number.

--- leetcode/py/p611-valid-triangle-number.py
+++ leetcode/py/p611-valid-triangle-number.py
@@ -6,55 +6,24 @@ from typing import List
 
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        """Third take.
+        """Second take. Runtime beats 49% of other submissions.
 
-        1. Skip invalid numbers with range instead of bisect_left.
-        2. Better hint for bounding the bisect_left call in invalid triangle search.
-
-        Runtime beats 51% of other submissions. Fine. Probably need a smarter math attack at this point.
+        Added triangles rather than subtracting out non-triangles.
         """
         nums.sort()
-
-        # Skip starting index past numbers < 1.
-        start = 0
-        for start in range(len(nums)):
-            if nums[start] > 0:
-                break
-
-        # Calculate possible number of triangles.
-        num_triangles = math.comb(len(nums) - start, 3)
-
-        # Subtract off invalid triangles
-        for a in range(start, len(nums) - 2):
-            c = a + 2
-            for b in range(a + 1, len(nums) - 1):
-                c = bisect.bisect_left(nums, nums[a] + nums[b], c)
-                if c == len(nums):
-                    break
-                num_triangles -= len(nums) - c
-        return num_triangles
-
-    def triangleNumber_take2(self, nums: List[int]) -> int:
-        """Second take.
-
-        Skip past invalid numbers rather than resize the list.
-
-        Runtime beats 47% of other submissions."""
-        nums.sort()
-
-        # Skip starting index past numbers < 1.
         start = bisect.bisect_left(nums, 1)
-
-        # Calculate possible number of triangles.
-        num_triangles = math.comb(len(nums) - start, 3)
-
-        # Subtract off invalid triangles
-        for a in range(start, len(nums) - 2):
-            for b in range(a + 1, len(nums) - 1):
-                c = bisect.bisect_left(nums, nums[a] + nums[b], b + 1)
+        if len(nums) - start < 3:
+            return 0
+        num_triangles = 0
+        for b in range(start + 1, len(nums) - 1):
+            c = b + 1
+            for a in range(start, b):
+                a_plus_b = nums[a] + nums[b]
+                c = bisect.bisect_left(nums, a_plus_b, lo=c)
                 if c == len(nums):
+                    num_triangles += (b - a) * (c - b - 1)
                     break
-                num_triangles -= len(nums) - c
+                num_triangles += c - b - 1
         return num_triangles
 
     def triangleNumber_take1(self, nums: List[int]) -> int:
@@ -90,6 +59,8 @@ if __name__ == "__main__":
         ([1, 2, 2, 0], 1),
         ([2, 2, 3, 4], 3),
         ([4, 2, 3, 4], 4),
+        ([1, 2, 4, 5, 6, 9], 5),
+        ([1, 2, 3, 4, 5, 6, 7], 13),
     )
     for nums, exp in tests:
         ans = s.triangleNumber(nums)
