6af8e5ac6eedf06080f2dd0c266915c5ae13ad25
Matt Drees <matt@drees.me>
2021-09-09 09:27:23 -0700

    LeetCode 953. Marginal performance improvement.
    pyperf says 10% speed up with large lists if we use a pre-build dictionary
    instead of string.order() lookup.

--- leetcode/py/p953-verifying-an-alien-dictionary.py
+++ leetcode/py/p953-verifying-an-alien-dictionary.py
@@ -1,9 +1,13 @@
 # LeetCode 953. Verifying an Alien Dictionary.
 from typing import List
+import pyperf
 
 
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        d = {}
+        for x, c in enumerate(order):
+            d[c] = x
         for x in range(0, len(words) - 1):
             w1 = words[x]
             w2 = words[x + 1]
@@ -14,7 +18,7 @@ class Solution:
                 if y < len_w1:
                     if y < len_w2:
                         if w1[y] != w2[y]:
-                            if order.index(w1[y]) < order.index(w2[y]):
+                            if d[w1[y]] < d[w2[y]]:
                                 break
                             else:
                                 return False
@@ -41,3 +45,108 @@ if __name__ == "__main__":
         ans = s.isAlienSorted(words, order)
         if exp != ans:
             print(f"FAIL. words: {words}, order: {order}, exp: {exp}, ans: {ans}")
+
+    perf_words = [
+        "abdominohysterectomy",
+        "acetylcholinesterase",
+        "acetylmethylcarbinol",
+        "adrenocorticosteroid",
+        "adrenocorticotrophic",
+        "adrenocorticotrophin",
+        "anarchoindividualist",
+        "anatomicochirurgical",
+        "anatomicophysiologic",
+        "anitinstitutionalism",
+        "anopisthographically",
+        "anthrohopobiological",
+        "anthropogeographical",
+        "anthropomorphisation",
+        "anthropomorphization",
+        "anthropomorphotheist",
+        "anthropophysiography",
+        "anthropoteleological",
+        "antianthropomorphism",
+        "antiaristocratically",
+        "antiauthoritarianism",
+        "anticapitalistically",
+        "anticonfederationism",
+        "anticonfederationist",
+        "anticonservativeness",
+        "anticonstitutionally",
+        "antiecclesiastically",
+        "antienthusiastically",
+        "antienvironmentalism",
+        "antienvironmentalist",
+        "antiinstitutionalist",
+        "antiinsurrectionally",
+        "antiinsurrectionists",
+        "antimilitaristically",
+        "antiparliamentarians",
+        "antiprestidigitation",
+        "antitintinnabularian",
+        "archaeopterygiformes",
+        "arteriosympathectomy",
+        "autodepolymerization",
+        "ballistocardiography",
+        "barothermohygrograph",
+        "benzofuroquinoxaline",
+        "biblicopsychological",
+        "blepharochromidrosis",
+        "blepharohematidrosis",
+        "bronchoaspergillosis",
+        "bronchoesophagoscopy",
+        "calcareoargillaceous",
+        "characteristicalness",
+        "chemicomineralogical",
+        "chemicophysiological",
+        "chemoautotrophically",
+        "chemotherapeutically",
+        "chemotherapeuticness",
+        "chlamydobacteriaceae",
+        "cholecystenterostomy",
+        "cholecystgastrostomy",
+        "cholecystnephrostomy",
+        "chronocinematography",
+        "cinephotomicrography",
+        "compartmentalization",
+        "consubstantiationist",
+        "counterdemonstration",
+        "counterdemonstrators",
+        "counterdisengagement",
+        "counterestablishment",
+        "counterexpostulation",
+        "counterrevolutionary",
+        "counterrevolutionist",
+        "counterrevolutionize",
+        "crystallographically",
+        "decahydronaphthalene",
+        "desoxycorticosterone",
+        "dimethylanthranilate",
+        "diphenylchloroarsine",
+        "diphenylquinomethane",
+        "disdenominationalize",
+        "disestablismentarian",
+        "disproportionateness",
+        "duodenojejunostomies",
+        "electroballistically",
+        "electrocardiographic",
+        "electrocauterization",
+        "electrochronographic",
+        "electrocontractility",
+        "electroencephalogram",
+        "electrogalvanization",
+        "electrohydraulically",
+        "electrometallurgical",
+        "electrophysiological",
+        "electropneumatically",
+        "electroretinographic",
+        "electrosynthetically",
+        "electrotherapeutical",
+        "encephalographically",
+        "encephalomeningocele",
+        "encephalomyocarditis",
+        "epididymodeferential",
+        "establismentarianism",
+    ]
+    runner = pyperf.Runner()
+    runner.bench_func("f", s.isAlienSorted, perf_words, "abcdefghijklmnopqrstuvwxyz")
