31a1153470c3d24bce23add7443eb1271990b818
Matt Drees <matt@drees.me>
2021-09-10 10:26:05 -0700

    LeetCode 848. Slightly faster code and added benchmark.
    Adding into a int64_t was much faster than doing a modulus on each value fo
    in32_t.

--- leetcode/cpp/p848-shifting-letters.cpp
+++ leetcode/cpp/p848-shifting-letters.cpp
@@ -1,4 +1,6 @@
 // LeetCode 848. Shifting letters.
+#include <benchmark/benchmark.h>
+
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -7,12 +9,41 @@ class Solution {
 public:
     std::string shiftingLetters(std::string s, const std::vector<int>& shifts)
     {
-        int carry = 0;
+        int64_t carry = 0;
         for (size_t i = shifts.size(); i-- > 0;) {
-            carry = (carry + shifts[i]) % 26;
+            carry += shifts[i];
             s[i] = (s[i] + carry - 97) % 26 + 97;
         }
         return s;
+    }
+};
+
+// Otherwise the "fastest" <cough> solution posted on leetcode for comparing benchmarks.
+class Solution2 {
+public:
+    std::string shiftingLetters(std::string s, std::vector<int>& shifts)
+    {
+        int len = shifts.size();
+        int sum = 0;
+        std::string res = "";
+        for (int num : shifts) {
+            num %= 26;
+            sum += num;
+            sum %= 26;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (i >= len)
+                return res;
+            int curr = s[i] - 'a';
+            curr += sum;
+            curr %= 26;
+            res += curr + 'a';
+            int temp = shifts[i] % 26;
+            sum -= temp;
+            if (sum < 0)
+                sum += 26;
+        }
+        return res;
     }
 };
 
@@ -27,7 +58,32 @@ static std::string to_string(const std::vector<int>& v)
     return oss.str();
 }
 
-int main()
+// Less than 1 billion but prime so interesting for shifting and getting different letters.
+const int large_prime_number = 179'424'691;
+
+static void BM_mine(benchmark::State& state)
+{
+    std::string s(state.range(0), 'a');
+    std::vector<int> shifts(state.range(0), large_prime_number);
+    Solution solution;
+    for (auto _ : state)
+        benchmark::DoNotOptimize(solution.shiftingLetters(s, shifts));
+}
+BENCHMARK(BM_mine)->Range(0, 10'000);
+
+static void BM_other(benchmark::State& state)
+{
+    std::string s(state.range(0), 'a');
+    std::vector<int> shifts(state.range(0), large_prime_number);
+    Solution2 solution;
+    for (auto _ : state)
+        benchmark::DoNotOptimize(solution.shiftingLetters(s, shifts));
+}
+BENCHMARK(BM_other)->Range(0, 10'000);
+
+BENCHMARK_MAIN();
+
+int main2()
 {
     struct TestCase {
         std::string s;
@@ -53,4 +109,5 @@ int main()
                       << "\n";
         }
     }
+    return 0;
 }
