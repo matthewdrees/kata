774a387bdfe16096f1db0670c5d26ed7367e9a22
Matt Drees <matt@drees.me>
2021-09-14 15:38:20 -0700

    LeetCode 1365. Speedup.

--- leetcode/py/p1365-how-many-numbers-etc.py
+++ leetcode/py/p1365-how-many-numbers-etc.py
@@ -7,37 +7,17 @@ import random
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         sorted_nums = sorted(nums)
-        d = {}
+        l = [0] * 101
         last_num = float("inf")
         for x in range(len(sorted_nums)):
             if sorted_nums[x] != last_num:
-                d[sorted_nums[x]] = x
-            last_num = sorted_nums[x]
-        output = [0] * len(nums)
-        for x in range(len(nums)):
-            output[x] = d[nums[x]]
-        return output
-
-
-# Copied from LeetCode. Indexes an array instead of using a dictionary. Smart.
-class Solution2:
-    def smallerNumbersThanCurrent(self, nums):
-        amt_of = [0] * 102
-        for n in nums:
-            amt_of[n] += 1
-        amt_less_than = [0] * 102
-        running_less_than = 0
-        for i in range(0, 101):
-            running_less_than += amt_of[i]
-            amt_less_than[i + 1] = running_less_than
-        output = []
-        for n in nums:
-            output.append(amt_less_than[n])
-        return output
+                l[sorted_nums[x]] = x
+                last_num = sorted_nums[x]
+        return [l[n] for n in nums]
 
 
 if __name__ == "__main__":
-    solution = Solution3()
+    solution = Solution()
     tests = (
         ([], []),
         ([1], [0]),
@@ -55,7 +35,7 @@ if __name__ == "__main__":
         if exp != ans:
             print(f"FAIL. nums: {nums}, exp: {exp}, ans: {ans}")
 
-    nums = [random.randint(0, 101) for x in range(2, 501)]
+    nums = [random.randint(0, 100) for x in range(2, 501)]
     # print(solution.smallerNumbersThanCurrent(nums))
     runner = pyperf.Runner()
     runner.bench_func("f", solution.smallerNumbersThanCurrent, nums)
