460d9b2f34ae4fbcc932382ee39f2a29c1ebd3c6
Matt Drees <matt@drees.me>
2021-11-03 13:36:46 -0700

    LeetCode 809. Refactor is_stretchy_word function.
    is_stretchy_word now takes a word instead of list of LetterGroup objects.
    
    Removed some superfluous print statements.

--- leetcode/py/p809-expressive-words.py
+++ leetcode/py/p809-expressive-words.py
@@ -20,7 +20,8 @@ def get_letter_groups(s: str):
     return lgs
 
 
-def is_stretchy_word(lgs1, lgs2):
+def is_stretchy_word(lgs1, word):
+    lgs2 = get_letter_groups(word)
     if len(lgs1) != len(lgs2):
         return False
     for lg1, lg2 in zip(lgs1, lgs2):
@@ -36,15 +37,8 @@ def is_stretchy_word(lgs1, lgs2):
 
 class Solution:
     def expressiveWords(self, s: str, words: List[str]) -> int:
-        lgs1 = get_letter_groups(s)
-        print(f"lgs1: {lgs1}")
-        num_stretchy_words = 0
-        for word in words:
-            lgs2 = get_letter_groups(word)
-            print(f"lgs2: {lgs2}")
-            if is_stretchy_word(lgs1, lgs2):
-                num_stretchy_words += 1
-        return num_stretchy_words
+        lgs = get_letter_groups(s)
+        return sum(1 for word in words if is_stretchy_word(lgs, word))
 
 
 if __name__ == "__main__":
