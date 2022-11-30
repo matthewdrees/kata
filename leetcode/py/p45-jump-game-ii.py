663858b5eafb0c77eb42202511e7147443ec8499
Matt Drees <matt@drees.me>
2022-11-29 20:24:35 -0800

    LeetCode 45, python, updated.

--- leetcode/py/p45-jump-game-ii.py
+++ leetcode/py/p45-jump-game-ii.py
@@ -6,6 +6,24 @@ import copy
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        ans = 0
+        farthest = 0
+        end = 0
+        for i in range(len(nums) - 1):
+            f = i + nums[i]
+            if farthest < f:
+                farthest = f
+            if farthest >= len(nums) - 1:
+                ans += 1
+                break
+            if i == end:
+                ans += 1
+                end = farthest
+        return ans
+
+
+class Solution2:
+    def jump(self, nums: List[int]) -> int:
         """First take."""
         if len(nums) <= 1:
             return 0
@@ -23,45 +41,13 @@ class Solution:
         return p[0]
 
 
-class Solution2:
-    def jump(self, nums: List[int]) -> int:
-        """Same as above, except consumes nums."""
-        if len(nums) <= 1:
-            return 0
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] == 0:
-                nums[i] = 10_000
-                continue
-            j_end = i + nums[i] + 1
-            if j_end >= len(nums):
-                nums[i] = 1
-            else:
-                nums[i] = min(nums[i + 1 : j_end]) + 1
-        return nums[0]
-
-
-class Solution3:
-    def jump(self, nums: List[int]) -> int:
-        """Copied from LeetCode solutions. Clever!"""
-        max_i = 0
-        total_j = 0
-        c = 0
-        for i in range(len(nums) - 1):
-            if i == c:
-                total_j += 1
-            if max_i < (i + nums[i]):
-                max_i = i + nums[i]
-            if i == c:
-                c = max_i
-        return total_j
-
-
 test_cases = (
     ([5], 0),
     ([1, 1], 1),
     ([1, 0], 1),
     ([1, 1, 7], 2),
     ([2, 1, 7], 1),
+    ([2, 3, 1], 1),
     ([2, 3, 1, 1, 4], 2),
     ([2, 3, 0, 1, 4], 2),
     ([5, 0, 0, 0, 0, 0], 1),
