de8d9a8aabcb89356d53f8511dba1e369f317060
Matt Drees <matt@drees.me>
2023-03-21 13:52:19 -0700

    First few chapters of fastapi tutorial
    First steps and path/query parameters.

--- courses/fastapi-tutorial/app/main.py
+++ courses/fastapi-tutorial/app/main.py
@@ -1,8 +1,40 @@
+from enum import Enum
 from fastapi import FastAPI
+from typing import Union
+import time
 
 app = FastAPI()
 
 
 @app.get("/")
 def hello_world():
-    return {"message": "OK"}
+    return {"message": "take 1"}
+
+@app.get("/items/{item_id}")
+def hello_world(item_id:str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if short:
+        item.update({"description": "This is a description"})
+    return item
+
+
+class AnimalName(str, Enum):
+    pig = "pig"
+    cow = "cow"
+
+@app.get("/animals/{animal_name}")
+def get_animal(animal_name:AnimalName):
+    if animal_name is AnimalName.pig:
+        message = "oink"
+    if animal_name is AnimalName.cow:
+        message = "moo"
+    return {"animal_name": animal_name, "message" : message}
+
+
+@app.get("/skipandlimit/")
+async def read_skipandlimit(skip: int = 0, limit: int = 10):
+    time.sleep(1.0)
+    return {"skip": skip, "limit": limit}
+
