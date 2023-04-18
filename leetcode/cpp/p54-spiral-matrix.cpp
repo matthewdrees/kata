3b64a89efad581bd47d7f34cc3778c58eca3db22
Matt Drees <matt@drees.me>
2023-04-18 16:03:38 -0700

    LeetCode 54, C++.

--- leetcode/cpp/p54-spiral-matrix.cpp
+++ leetcode/cpp/p54-spiral-matrix.cpp
@@ -1,11 +1,14 @@
 // LeetCode 54. Spiral Matrix.
+#include <benchmark/benchmark.h>
+
 #include "leetcode.hpp"
 
 #include <cassert>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
-class Solution {
+class Solution2 {
 public:
     std::vector<int> spiralOrder(const std::vector<std::vector<int>>& matrix)
     {
@@ -67,7 +70,97 @@ public:
     }
 };
 
-int main()
+class Solution {
+public:
+    std::vector<int> spiralOrder(const std::vector<std::vector<int>>& matrix)
+    {
+        size_t y_max = matrix.size();
+        assert(y_max >= 1);
+        size_t x_max = matrix[0].size();
+        assert(x_max >= 1);
+        size_t y_min = 0;
+        size_t x_min = 0;
+        std::vector<int> v(x_max * y_max);
+        auto it = v.begin();
+        while (true) {
+            // right
+            {
+                const auto& v2 = matrix[y_min];
+                it = std::copy(v2.begin() + x_min, v2.begin() + x_max, it);
+                if (it == v.end()) {
+                    break;
+                }
+                --x_max;
+            }
+
+            // down
+            for (size_t y = y_min + 1; y < y_max; ++y) {
+                *it++ = matrix[y][x_max];
+            }
+            if (it == v.end()) {
+                break;
+            }
+            --y_max;
+
+            // left
+            {
+                const auto& v2 = matrix[y_max];
+                it = std::reverse_copy(v2.begin() + x_min, v2.begin() + x_max, it);
+                if (it == v.end()) {
+                    break;
+                }
+            }
+
+            // up
+            for (size_t y = y_max - 1; y > y_min; --y) {
+                *it++ = matrix[y][x_min];
+            }
+            ++x_min;
+            ++y_min;
+            if (it == v.end()) {
+                break;
+            }
+        }
+        return v;
+    }
+};
+
+std::vector<std::vector<int>> gen_grid(size_t m, size_t n)
+{
+    std::vector<std::vector<int>> v;
+    int i = 0;
+    for (size_t y = 0; y < m; ++y) {
+        std::vector<int> v2(n);
+        std::iota(v2.begin(), v2.end(), i);
+        i += static_cast<int>(n);
+        v.push_back(std::move(v2));
+    }
+    return v;
+}
+
+static void BM_one(benchmark::State& state)
+{
+    const auto v = gen_grid(state.range(0), state.range(0));
+    Solution solution;
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(solution.spiralOrder(v));
+    }
+}
+BENCHMARK(BM_one)->Range(1, 10);
+
+static void BM_two(benchmark::State& state)
+{
+    const auto v = gen_grid(state.range(0), state.range(0));
+    Solution2 solution;
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(solution.spiralOrder(v));
+    }
+}
+BENCHMARK(BM_two)->Range(1, 10);
+
+BENCHMARK_MAIN();
+
+int main2()
 {
     struct TestCase {
         std::vector<std::vector<int>> matrix;
