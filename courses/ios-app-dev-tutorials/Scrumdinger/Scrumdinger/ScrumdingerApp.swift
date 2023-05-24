232645f256bcb1f414d00f90b0e0f17280af1b47
matthewdrees <matt@drees.me>
2023-05-24 11:11:43 -0700

    Scrumdinger project.

---
+++ courses/ios-app-dev-tutorials/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Matthew Drees on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
