dd15b72254834893b18b613bac429202e05556a7
matthewdrees <matt@drees.me>
2023-06-05 15:05:26 -0700

    IOS App Dev Tutorial: Creating the edit view.

--- courses/ios-app-dev-tutorials/Scrumdinger/Scrumdinger/Models/DailyScrum.swift
+++ courses/ios-app-dev-tutorials/Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -9,6 +9,14 @@ struct DailyScrum: Identifiable {
     var title: String
     var attendees: [Attendee]
     var lengthInMinutes: Int
+    var lengthInMinutesAsDouble: Double {
+        get {
+            Double(lengthInMinutes)
+        }
+        set {
+            lengthInMinutes = Int(newValue)
+        }
+    }
     var theme: Theme
 
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
@@ -30,7 +38,12 @@ extension DailyScrum {
             self.name = name
         }
     }
+
+    static var emptyScrum: DailyScrum {
+        DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
+    }
 }
+
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
