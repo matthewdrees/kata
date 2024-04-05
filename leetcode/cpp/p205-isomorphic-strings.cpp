a74dc64763238e79e7530613d339d2bcf5c53ea4
Matt Drees <matt@drees.me>
2024-04-05 14:20:26 -0700

    LeetCode 205, C++ alternate implementation.

--- leetcode/cpp/p205-isomorphic-strings.cpp
+++ leetcode/cpp/p205-isomorphic-strings.cpp
@@ -1,10 +1,35 @@
 // LeetCode 205. Isomorphic Strings.
+#include <cassert>
 #include <iostream>
 #include <unordered_map>
 
 class Solution {
 public:
     bool isIsomorphic(std::string s, std::string t)
+    {
+        char m1[128] = { 0 };
+        char m2[128] = { 0 };
+        for (size_t i = 0; i < s.size(); ++i) {
+            const auto c1 = s[i];
+            const auto c2 = t[i];
+            assert(c1 >= 0 && c1 < 128);
+            assert(c2 >= 0 && c2 < 128);
+            char& c1val = m1[static_cast<size_t>(c1)];
+            if (c1val != 0 && c1val != c2) {
+                return false;
+            }
+            char& c2val = m2[static_cast<size_t>(c2)];
+            if (c2val != 0 && c2val != c1) {
+                return false;
+            }
+            c1val = c2;
+            c2val = c1;
+        }
+        return true;
+    }
+
+    // First cut.
+    bool isIsomorphic2(std::string s, std::string t)
     {
         if (s.size() != t.size()) {
             return false;
@@ -38,8 +63,6 @@ int main()
     };
 
     TestCase test_cases[] = {
-        { "", "a", false },
-        { "a", "", false },
         { "", "", true },
         { "egg", "add", true },
         { "foo", "bar", false },
