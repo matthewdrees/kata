64c5bdab92f2aaf04735df20e4f017dbc9decb21
Matt Drees <matt@drees.me>
2024-04-05 12:35:27 -0700

    LeetCode 205, Python improvement.

--- leetcode/py/p205-isomorphic-strings.py
+++ leetcode/py/p205-isomorphic-strings.py
@@ -2,25 +2,20 @@
 
 
 class Solution:
-    def isIsomorphic2(self, s: str, t: str) -> bool:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         d1 = {}
         d2 = {}
         for c1, c2 in zip(s, t):
-            if c1 in d1:
-                if c2 != d1[c1]:
-                    return False
-                if c1 != d2.get(c2):
-                    return False
+            if c1 in d1 and d1[c1] != c2 or c2 in d2 and d2[c2] != c1:
+                return False
             else:
                 d1[c1] = c2
-                if c2 in d2:
-                    return False
                 d2[c2] = c1
         return True
 
-    def isIsomorphic(self, s: str, t: str) -> bool:
+    def isIsomorphic2(self, s: str, t: str) -> bool:
         """Copied from solutions. Brilliant!"""
         return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
 
