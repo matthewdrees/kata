e4dc9156baf148818304fceead5b0f0adec221b7
Matt Drees <matt@drees.me>
2024-05-13 14:43:25 -0700

    LeetCode 128, with Python pyperf for 3 solutions.

--- leetcode/py/p128-longest-consecutive-sequence.py
+++ leetcode/py/p128-longest-consecutive-sequence.py
@@ -5,7 +5,6 @@ from typing import List
 import pyperf
 import random
 
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums) == 0:
@@ -70,33 +69,29 @@ def test_longest_consecutive():
             print(f"fail. nums: {nums}, exp: {exp}, act: {act}")
 
 
-def get_perftest_nums():
+def get_perftest_nums(num_elements):
     nums = []
     random.seed(42)
-    for n in range(100_000):
+    for n in range(num_elements):
         nums.append(random.randint(-1_000_000_000, 1_000_000_000))
     # print(f"{nums[0:4]}")
     return nums
 
 
-def perf_longest_consective():
-    nums = get_perftest_nums()
-    solution = Solution()
-    runner = pyperf.Runner()
-    runner.bench_func(
-        "longestConsectutive, iterate over nums", solution.longestConsecutive, nums
-    )
-    runner.bench_func(
-        "longestConsectutive_iterate_over_set",
-        solution.longestConsecutive_iterate_over_set,
-        nums,
-    )
-    runner.bench_func(
-        "longestConsectutive_remove_from_set",
-        solution.longestConsecutive_remove_from_set,
-        nums,
-    )
+def perf_longest_consecutive_helper(runner, func_name, func):
+    for x in (0, 1, 2, 4, 8, 16):
+        num_elements = 2 ** x
+        nums = get_perftest_nums(num_elements)
+        runner.bench_func(f"{func_name}(n={num_elements})", func, nums)
 
+def perf_longest_consecutive():
+    runner = pyperf.Runner()
+    runner.parse_args()
+    solution = Solution()
+    perf_longest_consecutive_helper(runner, "longestConsecutive", solution.longestConsecutive)
+    perf_longest_consecutive_helper(runner, "longestConsecutive_iterate_over_set", solution.longestConsecutive_iterate_over_set)
+    perf_longest_consecutive_helper(runner, "longestConsecutive_remove_from_set", solution.longestConsecutive_remove_from_set)
 
 test_longest_consecutive()
-perf_longest_consective()
+
+perf_longest_consecutive()
